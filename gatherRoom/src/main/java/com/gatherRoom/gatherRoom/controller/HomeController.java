0b3e581e9417c7944e597c847f15ee5f2541be15
bigpond4932 <91193268+bigpond4932@users.noreply.github.com>
2022-07-17 21:44:54 +0900

    Revert "Revert "Update test2.txt""

--- gatherRoom/src/main/java/com/gatherRoom/gatherRoom/controller/HomeController.java
+++ gatherRoom/src/main/java/com/gatherRoom/gatherRoom/controller/HomeController.java
@@ -1,9 +1,7 @@
 package com.gatherRoom.gatherRoom.controller;
 
 import com.gatherRoom.gatherRoom.domain.ResponseTest;
-
 import lombok.extern.log4j.Log4j2;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
