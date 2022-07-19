811e4392a9f916dde61227115c523f3a716d979d
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2022-07-19 20:49:33 +0900

    구글 로그인 최종구현

---
+++ gatherRoom/src/main/java/com/gatherRoom/gatherRoom/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.gatherRoom.gatherRoom.config;
+
+import com.gatherRoom.gatherRoom.domain.Role;
+import com.gatherRoom.gatherRoom.service.oauth2.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+// 특정 주소로 접근하면 권한 및 인증을 미리 체크
+public class SecurityConfig extends WebSecurityConfigurerAdapter  {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+    }
+}
