759d7dbdb9d581902eeb8510cf9ef79ca1a80b00
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2022-07-26 22:57:44 +0900

    로그인, 회원가입 기능 구현

--- gatherRoom/src/main/java/com/gatherRoom/gatherRoom/controller/HomeController.java
+++ gatherRoom/src/main/java/com/gatherRoom/gatherRoom/controller/HomeController.java
@@ -1,15 +1,29 @@
 package com.gatherRoom.gatherRoom.controller;
 
+import com.gatherRoom.gatherRoom.service.oauth2.SessionUser;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @Log4j2
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(HttpServletRequest request) {
+//        HttpSession session = request.getSession();
+//        Enumeration<String> attributeNames = request.getSession().getAttributeNames();
+//        while(attributeNames.hasMoreElements()) {
+//            String attributeName = attributeNames.nextElement();
+//            Object attribute = session.getAttribute(attributeName);
+//            log.info("TEST :: attributeName = " + attributeName);
+//            log.info("TEST :: session = " + attribute);
+//            log.info("====================================================");
+//        }
+        SessionUser user = (SessionUser) request.getSession().getAttribute("user");
+        log.info("TEST :: user = " + user);
         log.info("/ - mainPage");
         return "index";
     }
--- gatherRoom/src/main/java/com/gatherRoom/gatherRoom/service/oauth2/SessionUser.java
+++ gatherRoom/src/main/java/com/gatherRoom/gatherRoom/service/oauth2/SessionUser.java
@@ -2,10 +2,12 @@ package com.gatherRoom.gatherRoom.service.oauth2;
 
 import com.gatherRoom.gatherRoom.domain.oauth2.User;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.io.Serializable;
 
 @Getter
+@ToString
 public class SessionUser implements Serializable {
 
     private String name;
